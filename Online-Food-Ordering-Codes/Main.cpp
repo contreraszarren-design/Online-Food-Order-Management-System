d0c9376fbdae237b30b0f340336766dfdfed7e74
andreiii122 <marcdrei143@gmail.com>
2026-05-02 18:51:13 +0800

    Initial Commit

--- Online-Food-Ordering-Codes/Main.cpp
+++ Online-Food-Ordering-Codes/Main.cpp
@@ -36,3 +36,32 @@ void addNormal() {
     normalQueue.push(o);
     cout << "Normal order added.\n";
 }
+void addPriority() {
+    Order o;
+    o.id = orderID++;
+    cout << "Enter customer name: ";
+    cin >> o.customer_Name;
+    cout << "Enter food item: ";
+    cin >> o.food_Item;
+    cout << "Enter priority (higher number = higher priority): ";
+    cin >> o.priority;
+
+    priorityQueue.push(o);
+    cout << "Priority order added.\n";
+}
+
+void processOrder() {
+    if (!priorityQueue.empty()) {
+        Order o = priorityQueue.top();
+        priorityQueue.pop();
+        history.push(o);
+        cout << "Processed priority order: " << o.customer_Name << " - " << o.food_Item << "\n";
+    } else if (!normalQueue.empty()) {
+        Order o = normalQueue.front();
+        normalQueue.pop();
+        history.push(o);
+        cout << "Processed normal order: " << o.customer_Name << " - " << o.food_Item << "\n";
+    } else {
+        cout << "No orders to process.\n";
+    }
+}
